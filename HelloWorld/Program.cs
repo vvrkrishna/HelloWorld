bccd8103a2341519e8a9624a56777a1a403f3e7a
Venkata Kambhampati <kambhampati@usf.edu>
2020-01-23 00:15:19 -0500

    Version3 of Hello World

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,10 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            //Pause to display Hello World!
+            Console.WriteLine("Press any key to continue..");
             Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
         }
     }
 }
